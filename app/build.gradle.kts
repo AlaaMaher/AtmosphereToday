0e192b721496c2bb092f5c14d14bf483b621359e
AlaaMaher <loaloa.maher@gmail.com>
2024-02-24 16:36:43 +0200

    adding dependancies and resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+    id ("kotlin-parcelize")
 }
 
 android {
@@ -66,4 +69,22 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Dagger - Hilt
+    implementation ("com.google.dagger:hilt-android:2.48")
+    kapt ("com.google.dagger:hilt-android-compiler:2.48")
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.2.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Location Services
+    implementation ("com.google.android.gms:play-services-location:20.0.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+
 }
--- app/src/main/java/com/alaa/atmospheretoday/ui/theme/Color.kt
+++ app/src/main/java/com/alaa/atmospheretoday/ui/theme/Color.kt
@@ -1,11 +1,14 @@
 package com.alaa.atmospheretoday.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val DarkPurple = Color(0xFF48319D)
+val LightPurple = Color(0xFF8362E3)
+val Gray = Color(0xFFEBEBF5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
